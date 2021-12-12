6e6e420d5787accbcfd05c97c1f64ab1783580ec
waad <iwaad1414@gmail.com>
2021-12-12 13:38:01 +0300

    splash

---
+++ app/src/main/java/com/example/finalcapstone_nomapp/main/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalcapstone_nomapp.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalcapstone_nomapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
