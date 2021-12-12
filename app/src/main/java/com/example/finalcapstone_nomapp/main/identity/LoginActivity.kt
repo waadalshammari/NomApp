343ffc5eb7e1aae252cdf2328d3d72a2f455d505
waad <iwaad1414@gmail.com>
2021-12-12 15:31:06 +0300

    commit 3 signUp

---
+++ app/src/main/java/com/example/finalcapstone_nomapp/main/identity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalcapstone_nomapp.main.identity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalcapstone_nomapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/finalcapstone_nomapp/main/identity/SignUpActivity.kt
@@ -0,0 +1,72 @@
+package com.example.finalcapstone_nomapp.main.identity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.finalcapstone_nomapp.R
+import com.example.finalcapstone_nomapp.databinding.ActivitySignUpBinding
+import com.example.finalcapstone_nomapp.main.view.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class SignUpActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+       // get context
+        val fullName : EditText = findViewById(R.id.fullname_editText_register)
+        val email : EditText = findViewById(R.id.email_editText_register)
+        val password : EditText = findViewById(R.id.password_editText_register)
+        val signupButton : Button = findViewById(R.id.register_button)
+        val signupTextview : TextView = findViewById(R.id.already_member_textView)
+
+
+          // make the button clickable
+        signupButton.setOnClickListener {
+            val emil : String = email.text.toString()
+            val password : String = password.text.toString()
+            val fullName :String = fullName.text.toString()
+
+
+            // make suer email&&password not blank and fullName not empty
+            if (emil.isNotBlank() && password.isNotBlank() && fullName.isNotEmpty()){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emil,password)
+
+                    .addOnCompleteListener(){
+                            task ->
+                        if (task.isSuccessful){
+                            val firebaseUser : FirebaseUser = task.result!!.user!!
+                            Toast.makeText(this,"User Registered Successfully", Toast.LENGTH_SHORT)
+                                .show()
+                            // Navigate to main Activity
+                            val intent = Intent(this,MainActivity::class.java)
+                            intent.putExtra("UserId", firebaseUser.uid)
+                            intent.putExtra("Email",firebaseUser.email)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this,task.exception!!.message.toString(), Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+            }
+        }
+            // make the text clickable and navigate to login activity
+        signupTextview.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        }
+
+        }
+
+
+    }
