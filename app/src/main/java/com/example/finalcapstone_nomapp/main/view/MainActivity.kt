f869d1c2db20e3c86b52898924e5169fec68af1d
waad <iwaad1414@gmail.com>
2021-12-13 20:38:11 +0300

    add bottom nav menu

--- app/src/main/java/com/example/finalcapstone_nomapp/main/view/MainActivity.kt
+++ app/src/main/java/com/example/finalcapstone_nomapp/main/view/MainActivity.kt
@@ -3,15 +3,33 @@ package com.example.finalcapstone_nomapp.main.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.finalcapstone_nomapp.R
+import com.example.finalcapstone_nomapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+// global variables for binding and nav
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-//        startActivity(Intent(this,LoginActivity::class.java))
-//        finish()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
 
     }
-}
+
+    }
