97578ceb9e2662a67438ec572a1e1532a2820114
waad <iwaad1414@gmail.com>
2021-12-14 21:07:10 +0300

    shimmer

--- app/src/main/java/com/example/finalcapstone_nomapp/main/view/RecipesFragment.kt
+++ app/src/main/java/com/example/finalcapstone_nomapp/main/view/RecipesFragment.kt
@@ -9,14 +9,15 @@ import com.example.finalcapstone_nomapp.R
 
 
 class RecipesFragment : Fragment() {
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        // for shimmer
+          view.recycler_view.showShimmer()
+        return view
     }
 
 
